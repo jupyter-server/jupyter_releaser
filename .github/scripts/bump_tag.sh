3b500ad47efcd908b926f91c4ebee8c891f6820e
Afshin T. Darian <git@darian.af>
2021-05-24 13:58:14 +0100

    Use Releaser to Tag Itself

---
+++ .github/scripts/bump_tag.sh
@@ -0,0 +1,7 @@
+set -eux
+
+# Update the v1 tag for GitHub Actions consumers
+if [[ ${RH_DRY_RUN:=true} != 'true' ]]; then
+    git tag -f -a v1 -m "Github Action release"
+    git push origin -f --tags
+fi
