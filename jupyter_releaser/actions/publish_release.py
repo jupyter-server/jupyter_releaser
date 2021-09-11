9ef5f63caed301fce1285c810ec8426f96b69d38
Jeremy Tuloup <jeremy.tuloup@gmail.com>
2021-09-11 13:49:29 +0200

    Add support for PYPI token map

--- jupyter_releaser/actions/publish_release.py
+++ jupyter_releaser/actions/publish_release.py
@@ -4,6 +4,17 @@ import os
 
 from jupyter_releaser.util import run
 
+# First extract the pypi token
+twine_pwd = os.environ.get("PYPI_TOKEN")
+pypi_token_map = os.environ.get("PYPI_TOKEN_MAP", "").replace(r"\n", "\n")
+if pypi_token_map:
+    for line in pypi_token_map.splitlines():
+        name, _, token = line.partition(",")
+        if name == os.environ["RH_REPOSITORY"]:
+            twine_pwd = token
+os.environ["TWINE_PASSWORD"] = token
+
+
 release_url = os.environ["release_url"]
 run(f"jupyter-releaser extract-release {release_url}")
 run(f"jupyter-releaser forwardport-changelog {release_url}")
