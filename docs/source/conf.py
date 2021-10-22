57328b422fe69d490d5cb15e08a569e95b08055c
GitHub Action <41898282+github-actions[bot]@users.noreply.github.com>
2021-10-22 14:23:55 +0000

    Publish 0.9.5
    SHA256 hashes:
    
    jupyter_releaser-0.9.5-py3-none-any.whl: 03f05e80175c7aed70f4f74130b5c3c1613afe29c2c4df6eaeae3e4088550357
    
    jupyter_releaser-0.9.5.tar.gz: 69759a4d144b52e4aae2be2280ee18c004206f9961e888b2494396f46800dbfe

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -23,7 +23,7 @@ copyright = "2021, Project Jupyter"
 author = "Project Jupyter"
 
 # The full version, including alpha/beta/rc tags.
-release = "0.9.4"
+release = "0.9.5"
 # The short X.Y version.
 version = ".".join(release.split(".")[:2])
 
