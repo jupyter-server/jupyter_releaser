f75afe83f3f9d3852ab0142ff1f10ecac174a975
hbcarlos <carlosherrerocontact@gmail.com>
2022-02-25 15:35:54 +0100

    pre-commit run --all-files

--- jupyter_releaser/changelog.py
+++ jupyter_releaser/changelog.py
@@ -83,7 +83,7 @@ def get_version_entry(
     branch = branch or util.get_branch()
     since = since or util.get_latest_tag(ref or branch, since_last_stable)
 
-    if since == "" :
+    if since == "":
         since = util.get_first_commit(ref or branch)
 
     util.log(f"Getting changes to {repo} since {since} on branch {branch}...")
