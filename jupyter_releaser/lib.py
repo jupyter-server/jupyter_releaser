f23e5088c47d6defeea2461a440a4263704ab8f8
Steven Silvester <steven.silvester@ieee.org>
2022-04-07 03:44:36 -0500

    go back to quiet

--- jupyter_releaser/lib.py
+++ jupyter_releaser/lib.py
@@ -54,7 +54,7 @@ def check_links(ignore_glob, ignore_links, cache_file, links_expire):
     python = sys.executable.replace(os.sep, "/")
     cmd = f"{python} -m pytest --noconftest --check-links --check-links-cache "
     cmd += f"--check-links-cache-expire-after {links_expire} "
-    cmd += "-raXs --color yes -vv "
+    cmd += "-raXs --color yes --quiet "
     cmd += f"--check-links-cache-name {cache_dir}/check-release-links "
     # do not run doctests, since they might depend on other state.
     cmd += "-p no:doctest "
