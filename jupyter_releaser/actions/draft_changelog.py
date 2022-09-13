7e4ad4b477c5460c342243c7fa7129d775d6c09d
Steven Silvester <steven.silvester@ieee.org>
2022-09-13 16:51:46 -0500

    More workflow cleanup (#380)

--- jupyter_releaser/actions/draft_changelog.py
+++ jupyter_releaser/actions/draft_changelog.py
@@ -1,11 +1,20 @@
 # Copyright (c) Jupyter Development Team.
 # Distributed under the terms of the Modified BSD License.
+import os
+
 from jupyter_releaser.actions.common import make_group, run_action, setup
-from jupyter_releaser.util import handle_since
+from jupyter_releaser.util import CHECKOUT_NAME, get_default_branch, handle_since
 
 setup()
 
 run_action("jupyter-releaser prep-git")
+
+# Handle the branch.
+if not os.environ.get("RH_BRANCH"):
+    cur_dir = os.getcwd()
+    os.chdir(CHECKOUT_NAME)
+    os.environ["RH_BRANCH"] = get_default_branch() or ""
+    os.chdir(cur_dir)
 
 # Capture the "since" variable in case we add tags before checking changelog
 # Do this before bumping the version.
