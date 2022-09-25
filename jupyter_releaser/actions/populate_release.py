a667911b7039a36f5bc6c8e8be5e01a301a638f0
Steven Silvester <steven.silvester@ieee.org>
2022-09-25 11:29:15 -0500

    fix handling of asset_shas check

--- jupyter_releaser/actions/populate_release.py
+++ jupyter_releaser/actions/populate_release.py
@@ -20,7 +20,9 @@ dry_run = os.environ.get("RH_DRY_RUN", "").lower() == "true"
 if not dry_run:
     # Skip if we already have asset shas.
     release_url = os.environ["RH_RELEASE_URL"]
-    gh = get_gh_object(False)
+    owner, repo = os.environ["RH_REPOSITORY"].split("/")
+    auth = os.environ["GITHUB_ACCESS_TOKEN"]
+    gh = get_gh_object(False, owner=owner, repo=repo, token=auth)
     release = release_for_url(gh, release_url)
     for asset in release.assets:
         if asset.name == "asset_shas.json":
