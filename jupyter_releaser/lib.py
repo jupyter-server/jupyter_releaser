71a417e7f393e1380efcb60f4d36536a0ebfc60c
Steven Silvester <steven.silvester@ieee.org>
2022-10-06 17:36:35 -0500

    add more debugging

--- jupyter_releaser/lib.py
+++ jupyter_releaser/lib.py
@@ -420,8 +420,10 @@ def prep_git(ref, branch, repo, auth, username, url):
         # Default to the GitHub Actions bot
         # https://github.community/t/github-actions-bot-email-address/17204/6
         git_user_name = username or "41898282+github-actions[bot]"
-        util.run(f'git config --global user.email "{git_user_name}@users.noreply.github.com"')
-        util.run(f'git config --global user.name "{git_user_name}"')
+        util.run(
+            f'git config --global user.email "{git_user_name}@users.noreply.github.com"', echo=True
+        )
+        util.run(f'git config --global user.name "{git_user_name}"', echo=True)
 
     # Set up the repository
     checkout_dir = os.environ.get("RH_CHECKOUT_DIR", util.CHECKOUT_NAME)
