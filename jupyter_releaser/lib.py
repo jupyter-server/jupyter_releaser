dff3d4a8ec4b7bd6e1ccd9111026a578942f21bb
Frédéric Collonval <fcollonval@users.noreply.github.com>
2023-01-09 11:40:08 +0100

    More type fix

--- jupyter_releaser/lib.py
+++ jupyter_releaser/lib.py
@@ -12,7 +12,7 @@ from datetime import datetime
 from glob import glob
 from pathlib import Path
 from subprocess import CalledProcessError
-from typing import Union
+from typing import Type, Union
 
 import mdformat
 from packaging.version import parse as parse_version
@@ -356,7 +356,7 @@ def publish_assets(
         util.log(f"Handling dist file {path}")
         suffix = Path(path).suffix
         if suffix in [".gz", ".whl"]:
-            dist: Union[SDist, Wheel]
+            dist: Union[Type[SDist], Type[Wheel]]
             if suffix == ".gz":
                 dist = SDist
             else:
