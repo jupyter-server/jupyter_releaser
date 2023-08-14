cadd9db79e094d9fdc4005243bcd8aa5efa3851c
Steven Silvester <steven.silvester@ieee.org>
2023-08-13 19:17:16 -0500

    cleanup

--- jupyter_releaser/util.py
+++ jupyter_releaser/util.py
@@ -602,26 +602,6 @@ def prepare_environment(fetch_draft_release=True):  # noqa
         if not os.path.exists(bare_repo):
             run(f"git init -b main --bare {url}")
             run(f"git remote add test {url}")
-            ref = os.environ.get("RH_REF", "")
-            ref_alias = ref
-            try:
-                run(f"git fetch origin --unshallow {branch}")
-            except Exception as e:
-                log(e)
-
-            # Handle the ref
-            if ref.startswith("refs/pull/"):
-                pull = ref[len("refs/pull/") :]
-                ref_alias = f"refs/pull/{pull}"
-            else:
-                ref = None
-
-            # Reuse existing branch if possible
-            if ref:
-                run(f"{GIT_FETCH_CMD} +{ref}:{ref_alias}")
-                run(f"{GIT_FETCH_CMD} {ref}")
-                run(f"git push test {ref}")
-
             run(f"git checkout {branch}")
             run(f"git push test {branch}")
         os.environ["RH_GIT_URL"] = url
